036dc450d7148a237efbb25fa165410f9184e14b
Keysya Aulia <if23.keysyaaulia@mhs.ubpkarawang.ac.id>
2025-07-24 15:42:44 +0700

    update style app

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -9,8 +9,8 @@
 
     {{-- job --}}
     <div class="d-flex justify-content-center align-items-center flex-column gap-3" style="min-height: 60vh;">
-        <a href="{{ route('wp.index') }}" class="btn btn-primary w-100 py-3 rounded-pill shadow-sm">
-            Scan Waiting Post
+        <a href="{{ route('wp.index') }}" class="btn w-100 py-3 rounded-pill shadow-sm" style="background-color: #2759cd;">
+            <b style="color: white">Scan Waiting Post</b>
         </a>
     </div>
 </div>
