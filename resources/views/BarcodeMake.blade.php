ff689ab076c1180b2be7d557eac10af7d8bddfa4
Keysya Aulia <if23.keysyaaulia@mhs.ubpkarawang.ac.id>
2025-07-31 09:57:53 +0700

    make qrcode for customers

--- resources/views/BarcodeMake.blade.php
+++ resources/views/BarcodeMake.blade.php
@@ -1,14 +1,35 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>QR Code List</title>
+    <style>
+        .qr-container {
+            display: flex;
+            flex-wrap: wrap;
+            gap: 30px;
+            padding: 20px;
+        }
+        .qr-box {
+            text-align: center;
+            border: 1px solid #ddd;
+            padding: 15px;
+            border-radius: 10px;
+            width: 200px;
+        }
+    </style>
+</head>
+<body>
 
-@php
-use Milon\Barcode\Facades\DNS1D;
-@endphp
+<h2 style="text-align: center;">Daftar QR Code</h2>
 
-@extends('layouts.app')
-
-@section('content')
-<div class="container text-center mt-5">
-    <h2>Barcode Generator</h2>
-    <p>{{ $data }}</p>
-    <img src="data:image/png;base64,{{ $barcode }}" alt="Barcode">
+<div class="qr-container">
+    @foreach ($dataList as $data)
+        <div class="qr-box">
+            {!! QrCode::size(150)->generate(json_encode($data)) !!}
+            <p><strong>{{ $data['customer'] }} -  {{ $data['route'] }} - {{ $data['cycle'] }}</strong></p>
+        </div>
+    @endforeach
 </div>
-@endsection
+
+</body>
+</html>
